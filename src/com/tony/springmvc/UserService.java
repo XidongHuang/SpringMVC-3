5738993aff89800d513fa85a3cb4be28941708fe
XidongHuang <gzink@live.cn>
2016-03-02 21:35:15 -0500

    SpringMVC
    1. Go through the process of SpringMVC
    2. SpringMVC and Spring

---
+++ src/com/tony/springmvc/UserService.java
@@ -0,0 +1,13 @@
+package com.tony.springmvc;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	
+	
+	public UserService() {
+		System.out.println("UserService Constructor....");
+	}
+}
